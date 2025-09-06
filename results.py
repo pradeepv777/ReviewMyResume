6ad875f03aeaac262e1b1b34ad793f6085b6350a
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2025-09-06 19:10:50 +0530

    Update results.py

--- results.py
+++ results.py
@@ -1,15 +1,19 @@
 import streamlit as st
+import pandas as pd
 
 def show_results(final_score, tier, breakdown, feedback):
     st.header("📊 Resume Analysis Results")
 
-    # Overall Score 
     st.subheader(f"🎯 Overall Score: {final_score}/100 (Tier {tier})")
-
-    st.subheader("Category-wise Breakdown")
-    st.table(breakdown)
-
-    # Feedback / Suggestions
-    st.subheader("💡 Suggestions & Feedback")
-    for f in feedback:
-        st.markdown(f"- {f}")
+    #here we split into 2 sections for table and feedback
+    col1, col2 = st.columns([1, 1])
+    # Left(Feedback)
+    with col1:
+        st.subheader("💡 Suggestions & Feedback")
+        for f in feedback:
+            st.markdown(f"- {f}")
+    #Right(Table)
+    with col2:
+        st.subheader("🗒️ Section-wise Breakdown")
+        df = pd.DataFrame(list(breakdown.items()), columns=["Category", "Score"])
+        st.table(df)  
